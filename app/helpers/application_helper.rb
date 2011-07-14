4d16ddefa9559b536bea8500b24bd501d2f3e3d4
Richard Hill <rhill@deseretbook.com>
2011-07-14 17:58:16 -0600

    Completed exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  # Return the logo link and image.
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 
   # Return a title on a per-page basis.
   def title
